bf94841963a4cad5ada4247db0ae10c57901b788
Faraj Fauzi <faraj.fauzi@gmail.com>
2015-06-02 23:12:33 +0700

    remove way generator

--- app/config/app.php
+++ app/config/app.php
@@ -122,8 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
-		'Dingo\Api\Provider\ApiServiceProvider',
-		'Prettus\Repository\RepositoryServiceProvider',
+		'Dingo\Api\Provider\ApiServiceProvider'
 
 	),
 
